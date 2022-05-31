8ee0ff0e90841d726944f568cc3670a36eec76e3
williamjbf <williamjbf@gmail.com>
2022-05-30 23:14:54 -0300

    project startup

---
+++ src/main/java/com/github/williamjbf/jsfstudy/UserBean.java
@@ -0,0 +1,26 @@
+package com.github.williamjbf.jsfstudy;
+
+import javax.inject.Named;
+
+@Named
+public class UserBean {
+
+    private String name;
+    private String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
